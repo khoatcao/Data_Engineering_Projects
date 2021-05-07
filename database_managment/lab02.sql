610d49085530d4066c77546a2668040105587246
khoa <caotoankhoa5@gmail.com>
2021-05-07 20:33:20 +0700

    commits

--- database_managment/lab02.sql
+++ database_managment/lab02.sql
@@ -23,3 +23,26 @@ use ABC;
 #where Suppliers.SupplierID = Products.SupplierID 
 #Group by Suppliers.SupplierID
 #having Max_Price - Min_Price <=  30;
+
+# 3) Xuất danh sách các hóa đơn (Id, OrderNumber, OrderDate) 
+#kèm theo tổng giá chi trả (UnitPrice*Quantity) cho hóa đơn đó, 
+#bên cạnh đó có cột Description là “VIP” nếu tổng giá lớn hơn 1500 và “Normal” nếu tổng giá nhỏ hơn 1500(Gợi ý: Dùng UNION)
+#select UnitPrice * Quantity, 'VIP' AS DESCRIPTION 
+#from `Order Details`
+#where UnitPrice * Quantity > 1500.0000
+#union 
+#select UnitPrice * Quantity, 'Normal' As Description 
+#from `Order Details`
+#where UnitPrice * Quantity < 1500.00000;
+
+# 4) Xuất danh sách những hóa đơn (Id, OrderNumber, OrderDate)
+# trong tháng 7 nhưng phải ngoại trừ ra những hóa đơn từ khách hàng France. (Gợi ý: dùng EXCEPT)
+#select OrderID, OrderDate, ShipCountry
+#from Orders
+#where  Month(OrderDate) = 07 and ShipCountry != "France";
+# 5) Xuất danh sách những hóa đơn (Id, OrderNumber, OrderDate, TotalAmount)  nào có TotalAmount nằm trong top 5 các hóa đơn. (Gợi ý : Dùng IN)
+#select OrderID as Order_ID,OrderDate as Order_Date, Freight
+#select * 
+#from Orders 
+#where Freight in (Select Freight from Orders group by Freight Order by Freight Desc)
+#limit 5;
