cd46ddbea45337476b54592ec200cf3c323d1008
khoa <caotoankhoa5@gmail.com>
2021-05-07 13:41:32 +0700

    commit

--- database_managment/lab02.sql
+++ database_managment/lab02.sql
@@ -5,11 +5,21 @@ use ABC;
 #kèm theo giá min và max của các sản phẩm mà nhà cung cấp đó cung cấp. 
 #Có sắp xếp theo thứ tự Id của nhà cung cấp (Gợi ý : Join hai bản Supplier và Product, dùng GROUP BY tính Min, Max)
 
-select Suppliers.SupplierID as Supplier_ID, Suppliers.CompanyName as Company_Name, Suppliers.ContactName as Contact_Name,Suppliers.City as City, Suppliers.Country,Suppliers.Phone,
-min(Products.UnitPrice) as Min_Price, max(Products.UnitPrice) as Max_Price 
+#select Suppliers.SupplierID as Supplier_ID, Suppliers.CompanyName as Company_Name, Suppliers.ContactName as Contact_Name,Suppliers.City as City, Suppliers.Country,Suppliers.Phone,
+#min(Products.UnitPrice) as Min_Price, max(Products.UnitPrice) as Max_Price 
 
-from Suppliers,Products
-where Suppliers.SupplierID = Products.SupplierID
+#from Suppliers,Products
+#where Suppliers.SupplierID = Products.SupplierID
 # the default in ascending order 
-group by Suppliers.SupplierID
-order by Suppliers.SupplierID
+#group by Suppliers.SupplierID
+#order by Suppliers.SupplierID
+
+# 2) 
+#Cũng câu trên nhưng chỉ xuất danh sách nhà cung cấp có sự khác biệt giá (max – min) không quá lớn (<=30).(Gợi ý: Dùng HAVING)
+#select Suppliers.SupplierId as Supplier_ID, Suppliers.CompanyName as Company_Name, Suppliers.ContactName as Contact_Name, Suppliers.City as City, Suppliers.Country, Suppliers.Phone, 
+#min(Products.UnitPrice) as Min_Price, max(Products.UnitPrice) as Max_Price
+
+#from Suppliers, Products
+#where Suppliers.SupplierID = Products.SupplierID 
+#Group by Suppliers.SupplierID
+#having Max_Price - Min_Price <=  30;
